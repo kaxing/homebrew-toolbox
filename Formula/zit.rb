c9cd1860e2af95d403df023ae1d1797050e22f3b
kaxing <kaxing@ymail.com>
2026-03-23 16:30:41 +0800

    chore(formula): sync 1

--- Formula/zit.rb
+++ Formula/zit.rb
@@ -1,8 +1,8 @@
 class Zit < Formula
   desc "Run Zig source files like scripts with caching"
   homepage "https://github.com/kaxing/zit"
-  url "https://github.com/kaxing/zit/archive/f53cd7046716e14639034f0217b5e8435d754854.tar.gz"
-  sha256 "4f0e086e1de15e219c56a77a7c82988c1ea17369e20b105c36e72efbc12c180f"
+  url "https://github.com/kaxing/zit/archive/99b4be1f75b6a925e4a371bee4f803439de45beb.tar.gz"
+  sha256 "4189250af360ae1f3cced149cd83eef48ffa6a91dc4c4b4ec7b48d0f9b173567"
   license "MIT"
 
   depends_on "zig" => :build
@@ -16,3 +16,4 @@ class Zit < Formula
     system "#{bin}/zit", "--clean"
   end
 end
+
