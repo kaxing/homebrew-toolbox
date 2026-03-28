aa6238fd7bcce56d6653237c3faf2ad4869aada6
kaxing <kaxing@ymail.com>
2026-03-28 09:29:43 +0800

    chore(formula): release zit v0.0.1

--- Formula/zit.rb
+++ Formula/zit.rb
@@ -1,5 +1,5 @@
 class Zit < Formula
-  desc "Run Zig source files like scripts with caching"
+  desc "CLI tool zit"
   homepage "https://github.com/kaxing/zit"
   version "0.0.1"
   license "MIT"
@@ -7,20 +7,20 @@ class Zit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kaxing/zit/releases/download/v0.0.1/zit-darwin-arm64.tar.gz"
-      sha256 "3ad645aa1ad191f0e089e3c224c210788bec811857370bc60ad624b24da4c5f9"
+      sha256 "ade952b8649a6f6c9b61920a6959249033ffb506142b5fcdf0c40a35347af037"
     else
       url "https://github.com/kaxing/zit/releases/download/v0.0.1/zit-darwin-amd64.tar.gz"
-      sha256 "5938225000598934cdadcb605c6328eff1d8c78421470d0593eb0ad19a0c8f4e"
+      sha256 "7b676c939104911cafa8999025ce3a27aa4b16b65c9bbbc35722ef729af187d6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kaxing/zit/releases/download/v0.0.1/zit-linux-arm64.tar.gz"
-      sha256 "b5afc16ff3c738024ce0316542adcb0918ce77bf982137f2840a4bca69cbc320"
+      sha256 "0624cd9c9d530ebd51cc6027963939b878d71baa879b5f4639da4b49ede9331c"
     else
       url "https://github.com/kaxing/zit/releases/download/v0.0.1/zit-linux-amd64.tar.gz"
-      sha256 "e08994586466da6bc956a89f17504fa5307e94230291e98bfcb507148481704e"
+      sha256 "5f5aae87c2420979f91f62a303e22952b3779ffb1d3acb19a6d3d057aac05ec6"
     end
   end
 
